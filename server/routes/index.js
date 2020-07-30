6434422456df3c284f9ce745d1ac570de753a6f7
poby123 <youngsm12@naver.com>
2020-07-31 00:40:51 +0900

    index page content added

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,23 +1,43 @@
-const express = require('express');
+var express = require('express');
+var mysql = require('mysql');
 const session = require('express-session');
-
-const sessionAuth = require('../config/session.js');
-const constData = require('../config/constants.js');
-
+var xl = require('excel4node'); /*https://www.npmjs.com/package/excel4node*/
 var router = express.Router();
 
+const dbConfig = require('../config/database.js');
+const sessionAuth = require('../config/session.js');
+const constData = require('../config/constants.js');
+const { render } = require('ejs');
+
+const connection = mysql.createPool(dbConfig);
 router.use(session(sessionAuth));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
   if(req.session.auth >= constData.writerAuth){
-    if(req.session.auth === constData.adminAuth){
-      res.render('index', {title : 'Manmin Youth', msg : "", menu:constData.adminNav});
-    }
-    else{
-      res.render('index', {title : 'Manmin Youth', msg : "", menu:constData.userNav});
-    }
+    let date = new Date();
+    let month = date.getMonth() + 1;
+    connection.query(`SELECT * FROM tblPlanner WHERE month = ? AND grade = ? `, [month, req.session.grade], (err, result)=> {
+      if(err){
+        console.log(err);
+        if(req.session.auth === constData.adminAuth){
+          res.render('index', {title : 'Manmin Youth', msg : "", result : 'null' , menu:constData.adminNav});
+        }
+        else{
+          res.render('index', {title : 'Manmin Youth', msg : "", result : 'null', menu:constData.userNav});
+        }
+      }else{
+        console.log(result);
+        if(req.session.auth === constData.adminAuth){
+          console.log('no error to get table');
+          res.render('index', {title : 'Manmin Youth', msg : "", result : result ,menu:constData.adminNav});
+        }
+        else{
+          res.render('index', {title : 'Manmin Youth', msg : "", result : result , menu:constData.userNav});
+        }
+      }
+    });
   }else{
     res.render('login', { title: 'Manmin Youth | Sigin in' , msg:"", menu:''});
   }
