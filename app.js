f8e6a083ed2d94891be45c55716c1c184ffb1bc6
poby123 <youngsm12@naver.com>
2020-07-31 00:11:18 +0900

    Year planner editor adding

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./server/routes/index');
 var authRouter = require('./server/routes/auth');
 var adminRouther = require('./server/routes/admin');
 var boardRouter = require('./server/routes/board');
+var plannerRouter = require('./server/routes/planner')
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/auth', authRouter);
 app.use('/admin', adminRouther);
 app.use('/board', boardRouter);
+app.use('/planner', plannerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/config/constants.js
+++ server/config/constants.js
@@ -4,12 +4,13 @@ module.exports = {
     adminNav : [
         {nav : "앱 사용자 편집", navLink : "/admin/editUser"},
         {nav : "기관원 목록 편집", navLink : "/admin/editClassMember"},
-        {nav : "데이터 보기", navLink : "/board/view"},
+        {nav : "서기 데이터 보기", navLink : "/board/view"},
         {nav : "기관 출석 체크", navLink : "/board"},
+        {nav : "연간 행사 편집", navLink : "/planner"},
         {nav : "로그아웃", navLink : "/auth/signout"},
     ],
     userNav : [
-        {nav : "데이터 보기", navLink : "/board/view"},
+        {nav : "서기 데이터 보기", navLink : "/board/view"},
         {nav : "기관 출석 체크", navLink : "/board"},
         {nav : "로그아웃", navLink : "/auth/signout"},
     ],
